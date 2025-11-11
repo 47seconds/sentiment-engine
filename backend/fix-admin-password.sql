733649e09efa6a9541bb7563caf917312679d174
Yash <yashs722005@gmail.com>
2025-11-11 14:35:51 +0530

    second1

--- backend/fix-admin-password.sql
+++
@@ -1,8 +0,0 @@
--- Fix admin password with correct BCrypt hash for "admin123"
-UPDATE users 
-SET password = '$2a$10$8kPbHFn5sGfU5Vwp8QpMTegQbODcVfSV2xVcMWw.hZAL9GVbL0g76' 
-WHERE email = 'admin@moveinsync.com';
-
-SELECT email, password, role, is_active 
-FROM users 
-WHERE email = 'admin@moveinsync.com';
--- backend/update-admin-simple-password.sql
+++
@@ -1,9 +0,0 @@
--- Update admin to use a simple password we can test
--- Password: Password123!
--- Hash generated from trusted source: $2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi
-
-UPDATE users 
-SET password = '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' 
-WHERE email = 'admin@moveinsync.com';
-
-SELECT email, password, role FROM users WHERE email = 'admin@moveinsync.com';
